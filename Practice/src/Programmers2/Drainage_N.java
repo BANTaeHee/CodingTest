114d4b71ca4e58da87ec305f4965b95e1f421154
BANTaeHee <ban7598@gmail.com>
2023-04-25 09:52:28 +0900

    [Programmers]Lv.0 n의 배수 (Java)

---
+++ Practice/src/Programmers2/Drainage_N.java
@@ -0,0 +1,23 @@
+package Programmers2;
+
+public class Drainage_N {
+
+	public static int solution(int num, int n) {
+        int answer = 0;
+        
+        if (num % n == 0) {
+        	answer = 1;
+        }else {
+        	answer = 0;
+        }
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		
+		int num = 98;
+		int n = 2;
+		
+		System.out.println(solution(num, n));
+	}
+}
