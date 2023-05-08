084a8e29069a64a8d958914a8143ed788a4ff11e
BANTaeHee <ban7598@gmail.com>
2023-05-08 23:04:16 +0900

    [Programmmers]문자열로 변환

---
+++ Practice/src/Programmers2/String_Change.java
@@ -0,0 +1,13 @@
+package Programmers2;
+
+public class Practice {
+	public static String solution(int n) {
+        String answer = "";
+        answer = Integer.toString(n);
+        return answer;
+    }
+	public static void main(String[] args) {
+		int n = 2573;
+		System.out.println(solution(n));
+	}
+}
